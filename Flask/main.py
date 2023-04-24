8d1aebb9bd15864cff5695e571334f0a47ee8e20
HaaniTharak <70282036+HaaniTharak@users.noreply.github.com>
2023-04-24 16:18:24 -0400

    Changes to /upload v10

--- Flask/main.py
+++ Flask/main.py
@@ -8,40 +8,39 @@ from google.cloud import storage
 
 app = create_app()
 
-@app.get("/")
+@app.route("/")
 def defaultPage():
     return render_template('mainPage.html')
 
 @app.get("/upload")
 def uploadPage():
-
-    key =
-    storage_client = storage.Client()
-    policy = storage_client.generate_signed_post_policy_v4(
-        "bucket-proto1",
-        "blob_name2",
-        expiration=datetime.timedelta(minutes=10),
-        fields={
-            'x-goog-meta-test': 'data'
-        }
-    )
-
-    # Create an HTML form with the provided policy
-    header = "<form action='{}' method='POST' enctype='multipart/form-data'>\n"
-    form = header.format(policy["url"])
-
-    # Include all fields returned in the HTML form as they're required
-    for key, value in policy["fields"].items():
-        form += f"  <input name='{key}' value='{value}' type='hidden'/>\n"
-
-    form += "  <input type='file' name='file'/><br />\n"
-    form += "  <input type='submit' value='Upload File' /><br />\n"
-    form += "</form>"
-
-    print(form)
-
-    return form
-    #return render_template('upload.html')
+    #
+    # storage_client = storage.Client()
+    # policy = storage_client.generate_signed_post_policy_v4(
+    #     "bucket-proto1",
+    #     "blob_name2",
+    #     expiration=datetime.timedelta(minutes=10),
+    #     fields={
+    #         'x-goog-meta-test': 'data'
+    #     }
+    # )
+    #
+    # # Create an HTML form with the provided policy
+    # header = "<form action='{}' method='POST' enctype='multipart/form-data'>\n"
+    # form = header.format(policy["url"])
+    #
+    # # Include all fields returned in the HTML form as they're required
+    # for key, value in policy["fields"].items():
+    #     form += f"  <input name='{key}' value='{value}' type='hidden'/>\n"
+    #
+    # form += "  <input type='file' name='file'/><br />\n"
+    # form += "  <input type='submit' value='Upload File' /><br />\n"
+    # form += "</form>"
+    #
+    # print(form)
+    #
+    # return form
+    return render_template('upload.html')
 
 
 if __name__ == "__main__":
