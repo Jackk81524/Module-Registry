7f55424493c0049413e3e32052aa6bf3f872ac9f
Jack Edward Knox <knox36@eceprog4.ecn.purdue.edu>
2023-04-26 18:36:53 -0400

    test sql

--- Flask/website/__init__.py
+++ Flask/website/__init__.py
@@ -8,53 +8,53 @@ from website.models.sql_table import db, Packages_table, add_package
 import requests
 import sqlalchemy
 
-# def getconn():
-#     with Connector() as connector:
-#         conn = connector.connect(
-#             "module-registry-ece461:us-central1:ece461-module-registry",
-#             "pymysql",
-#             user="461-user",
-#             password="461-test",
-#             db="Module-Registry",
-#             ip_type= IPTypes.PUBLIC 
-#         )
-#         return conn
-def connect_with_connector() -> sqlalchemy.engine.base.Engine:
-    """
-    Initializes a connection pool for a Cloud SQL instance of MySQL.
-
-    Uses the Cloud SQL Python Connector package.
-    """
-    # Note: Saving credentials in environment variables is convenient, but not
-    # secure - consider a more secure solution such as
-    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-    # keep secrets safe.
-
-    instance_connection_name = "module-registry-ece461:us-central1:ece461-module-registry"
-    db_user = "461-user"  # e.g. 'my-db-user'
-    db_pass = "461-test"  # e.g. 'my-db-password'
-    db_name = "Module-Registry"  # e.g. 'my-database'
-
-    ip_type = IPTypes.PUBLIC
-
-    connector = Connector(ip_type)
-
-    def getconn() -> pymysql.connections.Connection:
-        conn: pymysql.connections.Connection = connector.connect(
-            instance_connection_name,
+def getconn():
+    with Connector() as connector:
+        conn = connector.connect(
+            "module-registry-ece461:us-central1:ece461-module-registry",
             "pymysql",
-            user=db_user,
-            password=db_pass,
-            db=db_name,
+            user="461-user",
+            password="461-test",
+            db="Module-Registry",
+            ip_type= IPTypes.PUBLIC 
         )
         return conn
+# def connect_with_connector() -> sqlalchemy.engine.base.Engine:
+#     """
+#     Initializes a connection pool for a Cloud SQL instance of MySQL.
 
-    pool = sqlalchemy.create_engine(
-        "mysql+pymysql://",
-        creator=getconn
-        # ...
-    )
-    return pool
+#     Uses the Cloud SQL Python Connector package.
+#     """
+#     # Note: Saving credentials in environment variables is convenient, but not
+#     # secure - consider a more secure solution such as
+#     # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+#     # keep secrets safe.
+
+#     instance_connection_name = "module-registry-ece461:us-central1:ece461-module-registry"
+#     db_user = "461-user"  # e.g. 'my-db-user'
+#     db_pass = "461-test"  # e.g. 'my-db-password'
+#     db_name = "Module-Registry"  # e.g. 'my-database'
+
+#     ip_type = IPTypes.PUBLIC
+
+#     connector = Connector(ip_type)
+
+#     def getconn() -> pymysql.connections.Connection:
+#         conn: pymysql.connections.Connection = connector.connect(
+#             instance_connection_name,
+#             "pymysql",
+#             user=db_user,
+#             password=db_pass,
+#             db=db_name,
+#         )
+#         return conn
+
+#     pool = sqlalchemy.create_engine(
+#         "mysql+pymysql://",
+#         creator=getconn
+#         # ...
+#     )
+#     return pool
 
 
 def create_app():
