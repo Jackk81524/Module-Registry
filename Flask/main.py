1f3f8a462b187e8ede5a6145b0c63889b8e544c7
HaaniTharak <70282036+HaaniTharak@users.noreply.github.com>
2023-04-26 16:34:28 -0400

    Download From Bucket function implemented

--- Flask/main.py
+++ Flask/main.py
@@ -9,16 +9,18 @@ import base64
 import zipfile
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
-#ONLY for testing purposes on local machine. Private Key Grab and Authentication ONLY required to test on local machine. You need to have pKey.json in directory for below code to run.
+
+# ONLY for testing purposes on local machine. Private Key Grab and Authentication ONLY required to test on local machine. You need to have pKey.json in directory for below code to run.
 # client = storage.Client.from_service_account_json('pKey.json')
 # def uploadToBucket():
 #     bucket = client.get_bucket('bucket-proto1')
 #     blob = bucket.blob('myfileTest2')
 #     blob.upload_from_string("testingggg pls work")
 
-#Storing Files from memory onto Storage Bucket
+# Storing Files from memory onto Storage Bucket
 def uploadToBucket(contents, destination_blob_name, bucket_name='bucket-proto1'):
     # destination_blob_name = "storage-object-name"
     storage_client = storage.Client()
@@ -29,12 +31,24 @@ def uploadToBucket(contents, destination_blob_name, bucket_name='bucket-proto1')
         f"{destination_blob_name} with contents {contents} uploaded to {bucket_name}."
     )
 
+
+# This function returns the http download link for the user to download the module
+def downloadFromBucket(moduleName, bucketName='bucket-proto1'):
+    address = "https://storage.googleapis.com/"
+    address += bucketName + '/'
+    address += moduleName
+    return address
+
+
 app = create_app()
 BASE = 'https://module-registry-website-4a33ebcq3a-uc.a.run.app/'
 BASE = 'http://localhost:8000/'
+
+
 @app.route("/")
 def defaultPage():
     return render_template('mainPage.html')
+
 
 @app.route("/upload")
 def uploadPage():
@@ -43,63 +57,72 @@ def uploadPage():
 
 @app.route("/packagesListInput")
 def packagesListInput():
-    return send_from_directory('templates','packages.html')
+    return send_from_directory('templates', 'packages.html')
+
 
 # Post request that retrieves array from /pacakgesListInput, called under action in html file
-@app.route("/packagesList",methods = ["POST"])
+@app.route("/packagesList", methods=["POST"])
 def packagesListDisplay():
     data = request.form.get("Query")
     headers = {'Content-Type': 'application/json'}
     if data == "[*]":
-        response = requests.post(BASE + 'packages',json = {'PackageQuery' : ["*"]},headers = headers)
+        response = requests.post(BASE + 'packages', json={'PackageQuery': ["*"]}, headers=headers)
     else:
-        response = requests.post(BASE + 'packages',json = {'PackageQuery' : json.loads(data)},headers = headers)
+        response = requests.post(BASE + 'packages', json={'PackageQuery': json.loads(data)}, headers=headers)
     # print(response)
     return response.json(), response.status_code
     # return 'test'
 
+
 @app.route("/toResetRegistry")
 def checkReset():
-    return send_from_directory('templates','reset.html')
+    return send_from_directory('templates', 'reset.html')
 
-@app.route("/RegistryReset",methods=["POST","DELETE"])
+
+@app.route("/RegistryReset", methods=["POST", "DELETE"])
 def ResetRegistry():
     delete = requests.delete(BASE + 'reset')
     return delete.json(), delete.status_code
 
+
 @app.route("/getPackageID")
 def getID():
-    return send_from_directory('templates','packageID.html')
+    return send_from_directory('templates', 'packageID.html')
 
-@app.route("/packageIDQuery",methods=["POST","GET"])
+
+@app.route("/packageIDQuery", methods=["POST", "GET"])
 def displayID():
     ID = request.form.get("ID")
-    data = requests.get(BASE + 'package/'+ID)
+    data = requests.get(BASE + 'package/' + ID)
     return data.json(), data.status_code
 
-@app.route("/packageIDDelete",methods=["POST","DELETE"])
+
+@app.route("/packageIDDelete", methods=["POST", "DELETE"])
 def deleteID():
     ID = request.form.get("ID")
-    data = requests.delete(BASE + 'package/'+ID)
+    data = requests.delete(BASE + 'package/' + ID)
     return data.json(), data.status_code
+
 
 @app.route("/packageRateID")
 def getRateID():
-    return send_from_directory('templates','rateID.html')
+    return send_from_directory('templates', 'rateID.html')
 
-@app.route("/packageIDRate",methods=["POST","DELETE"])
+
+@app.route("/packageIDRate", methods=["POST", "DELETE"])
 def RateID():
     ID = request.form.get("Rate")
     data = requests.get(BASE + 'package/' + ID + "/rate")
     return data.json(), data.status_code
 
-#@bp.get("/upload")
-#def toUpload():
-    #print("testing file")
-    #return render_template('mainPage.html')
-    #return send_from_directory('templates','upload.html')
 
-@app.route("/uploadContent", methods = ["POST"])
+# @bp.get("/upload")
+# def toUpload():
+# print("testing file")
+# return render_template('mainPage.html')
+# return send_from_directory('templates','upload.html')
+
+@app.route("/uploadContent", methods=["POST"])
 def handleUploaded():
     URL = request.form.get("URL")
     ZipFile = request.files.get("File")
@@ -107,10 +130,10 @@ def handleUploaded():
     if len(URL) != 0 and ZipFile.read() != b"":
         abort(400)
     elif URL != "":
-        response = requests.post(BASE + 'package',json = {'URL' : URL,'ZipFile' : None},headers = headers)
+        response = requests.post(BASE + 'package', json={'URL': URL, 'ZipFile': None}, headers=headers)
     elif ZipFile != None:
         ZipFile_string = base64.b64encode(ZipFile.read()).decode('utf-8')
-        response = requests.post(BASE + 'package',json = {'URL' : None,'ZipFile' : ZipFile_string},headers = headers)
+        response = requests.post(BASE + 'package', json={'URL': None, 'ZipFile': ZipFile_string}, headers=headers)
     else:
         abort(501)
     return response.json(), response.status_code
@@ -118,4 +141,4 @@ def handleUploaded():
 
 if __name__ == "__main__":
     # app.register_blueprint(bp)
-    app.run(host="localhost",port = 8000, debug=True)
+    app.run(host="localhost", port=8000, debug=True)
