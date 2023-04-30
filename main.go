a724bf5fcc3e5df64efe4971b0f54f8df8d86c47
Jack Knox <knox36@purdue.edu>
2023-04-30 19:24:05 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
+package main
 
+import (
+	"fmt"
+	//https://linguinecode.com/post/how-to-import-local-files-packages-in-golang
+	"github.com/KevinMi2023p/ECE461_TEAM33/package_analyzer"
+)
+
+// main function will handle the command line arguments
+func main() {
+  fmt.Println();
+}
